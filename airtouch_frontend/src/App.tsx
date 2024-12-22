424d03f15eda037143597a942e4c3a1922cd56ad
ranaj007 <ajrandell01@gmail.com>
2024-12-23 03:14:58 +1030

    Reformatted elements, removed presets, added total to prevent spill mode

--- airtouch_frontend/src/App.tsx
+++ airtouch_frontend/src/App.tsx
@@ -6,9 +6,47 @@ function App() {
   const [data, setData] = useState(['test', 'test2']);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [value, setValue] = useState(50);
   const [zonePercentages, setZonePercentages] = useState({});
   const [zoneTemps, setZoneTemps] = useState({});
+  const [ventTotal, setVentTotal] = useState(0);
+
+  const thumbColor = ventTotal >= 100 ? '#5fc998' : '#61a1fe';
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('api/get_zones');
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const result = await response.json();
+      setData(result['zones']);
+      setZonePercentages(result['zone_percents']);
+      setZoneTemps(result['zone_temps']);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const setZones = async () => {
+    try {
+      const response = await fetch('api/set_zones', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(zonePercentages),
+      });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const result = await response.json();
+      setZonePercentages(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   const setZonePercent = (zone: string, percent: number) => {
     setZonePercentages({
@@ -17,53 +55,44 @@ function App() {
     });
   };
 
+  const calcVentTotal = () => {
+    let total = 0;
+    for (const zone in zonePercentages) {
+      total += zonePercentages[zone];
+    }
+    total = Math.min(total, 100);
+    setVentTotal(total);
+  }
+
   const presets = [0, 10, 25, 100];
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('api/get_zones');
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
-        const result = await response.json();
-        setData(result['zones']);
-        setZonePercentages(result['zone_percents']);
-        setZoneTemps(result['zone_temps']);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchData();
   }, []);
 
+  useEffect(() => {
+    calcVentTotal();
+  }, [zonePercentages]);
+
   return (
-    <div className="mx-2">
+    <Container className="mx-0">
       <h1>AirTouch Controller</h1>
         {loading && <p>Loading...</p>}
         <Row>
-          <Col>
-          <Row>
             <Col xs="2"><h4>Zone</h4></Col>
-            <Col xs="1"><h4>Temp</h4></Col>
-            <Col xs="1"><h4>Vent</h4></Col>
-            <Col md={{offset: 1}}><h4>Set</h4></Col>
-            <Col className='ms-5'><h4>Presets</h4></Col>
-          </Row>
-          </Col>
+            <Col xs="2"></Col>
+            <Col xs="2"></Col>
+            <Col className="text-center"><h4>Vent</h4></Col>
+            <Col xs="1"></Col>
         </Row>
         <Row>
-
           {data && (
             <Col>
               {data.map((zone: any) => (
                 <Row key={zone} className="mb-2">
                   <Col xs="2">{zone}</Col>
-                  <Col xs="1">{zoneTemps[zone] && <div>{zoneTemps[zone]}°C</div>}</Col>
-                  <Col xs="auto">{zonePercentages[zone]}%</Col>
+                  <Col xs="2">{zoneTemps[zone] && <div>{zoneTemps[zone]}°C</div>}</Col>
+                  <Col xs="2">{zonePercentages[zone]}%</Col>
                   <Col>
                     <Form.Range
                       value={zonePercentages[zone]}
@@ -73,17 +102,42 @@ function App() {
                       step={5}
                     />
                   </Col>
-                  {presets.map((preset) => (
-                    <Col className="px-1" key={`${zone}-${preset}`} xs="auto">
-                      <Button key={preset} onClick={() => setZonePercent(zone, preset)}>{preset}</Button>
-                    </Col>
-                  ))}
+                  <Col xs="1"></Col>
                 </Row>
               ))}
             </Col>
           )}
         </Row>
-    </div>
+        <Row>
+            <Col xs="2">Total</Col>
+            <Col xs="2"></Col>
+            <Col xs="2">{ventTotal}%</Col>
+            <Col>
+            <Form.Range
+            className='custom-range'
+            style={{
+              '--thumb-color': thumbColor,
+            }}
+              value={ventTotal}
+                      min={0}
+                      max={100}
+                      step={5}
+                    />
+            </Col>
+            <Col xs="1"></Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs="auto">
+            <Button
+              onClick={setZones}
+              disabled={ventTotal < 100}
+            >
+              Set Vents
+            </Button>
+          </Col>
+
+        </Row>
+    </Container>
   )
 }
 
